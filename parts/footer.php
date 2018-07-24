eb08e27161d16a5335dc8290257d1fab2840e9bc
Groutch <hurizen1@hotmail.com>
2018-07-24 11:32:40 +0200

    instruction 2: header: link style.css include menu | footer: link app.js | home: include header+footer

--- parts/footer.php
+++ parts/footer.php
@@ -1,1 +1,59 @@
-put some footer here
+<footer class="page-footer font-small ">
+    <!-- Footer Elements -->
+    <div class="container">
+
+        <!-- Grid row-->
+        <div class="row">
+
+            <!-- Grid column -->
+            <div class="col-md-12 py-5">
+                <div class="mb-5 flex-center">
+
+                    <!-- Facebook -->
+                    <a class="fb-ic">
+                        <i class="fab fa-facebook fa-lg mr-md-5 mr-3 fa-2x"> </i>
+                    </a>
+                    <!-- Twitter -->
+                    <a class="tw-ic">
+              <i class="fab fa-twitter fa-lg mr-md-5 mr-3 fa-2x"> </i>
+            </a>
+                    <!-- Google +-->
+                    <a class="gplus-ic">
+              <i class="fab fa-google-plus fa-lg mr-md-5 mr-3 fa-2x"> </i>
+            </a>
+                    <!--Linkedin -->
+                    <a class="li-ic">
+              <i class="fab fa-linkedin fa-lg mr-md-5 mr-3 fa-2x"> </i>
+            </a>
+                    <!--Instagram-->
+                    <a class="ins-ic">
+              <i class="fab fa-instagram fa-lg mr-md-5 mr-3 fa-2x"> </i>
+            </a>
+                    <!--Pinterest-->
+                    <a class="pin-ic">
+              <i class="fab fa-pinterest fa-lg fa-2x"> </i>
+            </a>
+                </div>
+            </div>
+            <!-- Grid column -->
+
+        </div>
+        <!-- Grid row-->
+
+    </div>
+    <!-- Footer Elements -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2018 Copyright:
+        <a href="https://mdbootstrap.com/bootstrap-tutorial/"> MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+
+</footer>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js"></script>
+<script src="../public/app.js"></script>
+</body>
+
+</html>
--- parts/header.php
+++ parts/header.php
@@ -1,1 +1,15 @@
-put some header here
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css">
+    <link rel="stylesheet" href="../public/style.css">
+    <title> Site Perso </title>
+</head>
+
+<body>
+    <?php include('menu.php'); ?>
