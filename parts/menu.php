40c84ef9f0712e2ac5773f8a2dddfa2fc497d9b9
Groutch <hurizen1@hotmail.com>
2018-07-24 14:29:36 +0200

    instruction 6: Affichage sur page admin ajout bouton admin sur index

--- parts/menu.php
+++ parts/menu.php
@@ -1,27 +1,30 @@
-
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="/public">Site Perso</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand" href="/public">Site Perso</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-      </li>
-    </ul>
-    <ul class="navbar-nav my-2 my-lg-0">
-      <li class="nav-item">
-        <form action="/public/index.php" method=GET>
-            <input hidden type="text" value="bio" name="page">
-            <button class="btn btn-primary" id="page">Bio</button>
-        </form>
-      </li>
-        <li class="nav-item">
-        <form action="/public/index.php" method=GET>
-            <input hidden type="text" value="contact" name="page">
-            <button class="btn btn-primary" id="page">Contact</button>
-        </form>
-      </li>
-  </div>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+            </li>
+        </ul>
+        <ul class="navbar-nav my-2 my-lg-0">
+            <li class="nav-item">
+                <form action="/public/index.php" method=GET>
+                    <input hidden type="text" value="bio" name="page">
+                    <button class="btn btn-primary" id="page">Bio</button>
+                </form>
+            </li>
+            <li class="nav-item">
+                <form action="/public/index.php" method=GET>
+                    <input hidden type="text" value="contact" name="page">
+                    <button class="btn btn-primary" id="page">Contact</button>
+                </form>
+            </li>
+            <li class="nav-item">
+                <a href="/public/admin.php"><button class ="btn btn-primary" id="admin">Admin</button></a>
+            </li>
+        </ul>
+    </div>
 </nav>
